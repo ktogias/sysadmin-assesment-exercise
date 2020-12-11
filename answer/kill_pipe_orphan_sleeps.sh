87a0f371df54c5fdac8abc28578586f12f68d668
ktogias <ktogias@inspiron.proastio.patra>
2020-12-11 17:09:51 +0200

    Implemented get_pipe_pids() and contains_runner_pid to support filtering sleep pids to kill

--- answer/kill_pipe_orphan_sleeps.sh
+++ answer/kill_pipe_orphan_sleeps.sh
@@ -14,6 +14,31 @@ filter_infinity_sleep_process_ids () {
     done
 }
 
+get_pipe_pids (){
+    local PIPE_LS="$1"
+    PIPE_PIDS=()
+    for WORD in $PIPE_LS;
+    do
+        local PID=$(echo $WORD | sed '/\/proc\/\([0-9]*\)\/fd.*/!d;s//\1/')
+        if [ "$PID" != "" ]; then
+            PIPE_PIDS+=( $PID )
+        fi
+    done
+}
+
+contains_runner_pid (){
+    local PIDS="$@"
+    for PID in ${PIDS[@]}; do
+        local COMMAND=$(ps -p "$PID" -o args --no-headers)
+        local MATCH=$(echo $COMMAND | sed '/.*\(runner\.sh\ FAKESMS_START.*FAKESMS_END\)/!d;s//\1/')
+        if [ "$MATCH" != "" ]; then
+            echo "1"
+            return 0
+        fi
+    done
+    echo "0"
+}
+
 filter_to_kill_process_ids (){
     local ALL_PIDS="$@"
     TO_KILL_PIDS=()
@@ -21,8 +46,11 @@ filter_to_kill_process_ids (){
     do
         local PIPE=$(readlink -f /proc/"$PID"/fd/1)
         local PIPE_PART=$(echo $PIPE | sed 's/.*\(pipe:.*\)/\1/' )
-        local PIPE_PIDS=$((find /proc -type l | xargs ls -l | fgrep "$PIPE_PART") 2>/dev/null)
-        TO_KILL_PIDS+=( $PID )
+        local PIPE_LS="$((find /proc -type l | xargs ls -l | fgrep "$PIPE_PART") 2>/dev/null)"
+        get_pipe_pids "$PIPE_LS"
+        if [ "$(contains_runner_pid "${PIPE_PIDS[@]}")" != "1" ]; then
+            TO_KILL_PIDS+=( $PID )
+        fi;
     done
 }
 
